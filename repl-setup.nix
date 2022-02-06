d60be82373456ffbac7fec6e11e6344abfec66d5
Anselm Schüler <mail@anselmschueler.com>
2022-02-06 13:00:38 +0100

    More things added

--- repl-setup.nix
+++ repl-setup.nix
@@ -1,8 +1,8 @@
 with builtins;
 let
   self = args@{ configPath ? "/etc/nixos", inputName ? "nixpkgs"
-    , system ? builtins.currentSystem, passInput ? true, passPkgs ? true
-    , passExtra ? [ "lib" ] }:
+    , hostname ? null, system ? builtins.currentSystem, passInput ? true
+    , passPkgs ? true, passExtra ? [ "lib" ] }:
     let
       extra = builtins.listToAttrs (map (k: {
         name = k;
@@ -11,9 +11,22 @@ let
       payload = {
         repl-setup = { inherit args self payload extra; };
         seq-pkgs = seq payload.pkgs true;
-        config = getFlake configPath;
+        configFlake = getFlake configPath;
+        configs = payload.configFlake.nixosConfigurations or { };
+        hostname = if hostname != null then
+          hostname
+        else if payload.configs != { } then
+          head (attrNames payload.configs)
+        else
+          null;
+        ${
+          if payload.hostname != null && payload.configs ? ${hostname} then
+            "configuration"
+          else
+            null
+        } = payload.configs.${hostname};
         ${if passInput then inputName else null} =
-          payload.config.inputs.${inputName};
+          payload.configFlake.inputs.${inputName};
         ${if passPkgs then "pkgs" else null} =
           payload.${inputName}.legacyPackages.${system};
       } // extra;
