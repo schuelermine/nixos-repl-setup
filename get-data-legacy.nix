439c5785014a63729c94560e02b2d60a283347e0
Anselm Schüler <mail@anselmschueler.com>
2022-02-09 18:20:10 +0100

    start of revamp

---
+++ get-data-legacy.nix
@@ -0,0 +1,1 @@
+files: { source, ... }: null
--- repl-setup.nix
+++ repl-setup.nix
@@ -1,34 +1,6 @@
 with builtins;
-let
-  self = args@{ configPath ? "/etc/nixos", inputName ? "nixpkgs"
-    , hostname ? null, system ? builtins.currentSystem, passInput ? true
-    , passPkgs ? true, passExtra ? [ "lib" ] }:
-    let
-      extra = builtins.listToAttrs (map (k: {
-        name = k;
-        value = payload.${inputName};
-      }) passExtra);
-      payload = {
-        repl-setup = { inherit args self payload extra; };
-        seq-pkgs = seq payload.pkgs true;
-        configFlake = getFlake configPath;
-        configs = payload.configFlake.nixosConfigurations or { };
-        hostname = if hostname != null then
-          hostname
-        else if payload.configs != { } then
-          head (attrNames payload.configs)
-        else
-          null;
-        ${
-          if payload.hostname != null && payload.configs ? ${hostname} then
-            "configuration"
-          else
-            null
-        } = payload.configs.${hostname};
-        ${if passInput then inputName else null} =
-          payload.configFlake.inputs.${inputName};
-        ${if passPkgs then "pkgs" else null} =
-          payload.${inputName}.legacyPackages.${system};
-      } // extra;
-    in payload;
-in self
+args@{ source ? "/etc/nixos", ... }:
+let files = readDir source;
+in import ./process-data.nix (import
+  (if files ? "flake.nix" then ./get-data-flake.nix else ./get-data-legacy.nix)
+  files args)
